b2c3844ddcf8de963e161da030642f3fb31528b4
NiuFuyu855 <103884299+NiuFuyu855@users.noreply.github.com>
2022-05-09 23:50:03 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "9dc0c6aa-39a1-46be-9938-85999e3c3498",
                         "alterId": 0
                     }
                 ],
